685d22508f01b82449a45e56f6bd6452751a9a27
rboxochir <erdeneochir0417@gmail.com>
2019-01-16 16:55:16 +0800

    "shinechlev"

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,1 +1,18 @@
-var eml = document.getElementById('email').value;
+var surname = document.getElementById("");
+var name = document.getElementById("");
+var email = document.getElementById("");
+var phone = document.getElementById("");
+var address = document.getElementById("");
+var bith = document.getElementById("");
+var regnum = document.getElementById("");
+var reglet = "АБВГДЕЁЖЗИЙКЛМНОӨПРСТУҮФХЦЧШЩЬЫЪЭЮЯ";
+var num = "0123456789";
+
+if(surname === ""){
+    return false
+};
+
+if(name === ""){
+    return false;
+};
+
